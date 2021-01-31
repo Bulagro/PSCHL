478e391865b053db99f2b683d8e29b13127a9baf
Bulagro <christumer@gmail.com>
2021-01-30 21:37:02 -0600

    Implemented basic identifier tokenization.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
 use serde::Deserialize;
+
+const IDENTIFIER_CHARS: &str = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ_";
 
 #[derive(Debug, PartialEq)]
 pub enum Type {
@@ -12,18 +13,13 @@ pub enum Type {
     Delimiter,
     Identifier,
     NewLine,
+    None,
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Token {
     pub t: Type,
     pub c: String,
-}
-
-impl Token {
-    pub fn new(t: Type, c: String) -> Self {
-        Token {t, c}
-    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -34,12 +30,31 @@ struct Keywords {
 }
 
 pub fn tokenize<'a> (input_str: &'a str, lang_config_str: &'static str) -> Vec<Token> {
-    if input_str == "" {}
+    let _keywords: Keywords = serde_json::from_str(lang_config_str).unwrap();
 
-    let keywords: Keywords = serde_json::from_str(lang_config_str).unwrap();
-    let tokens: Vec<Token> = Vec::new();
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut token_type: Type = Type::None;
+    let mut token_content: String = String::new();
 
+    for c in input_str.chars() {
+        if IDENTIFIER_CHARS.contains(c) {
+            if token_content.is_empty() {
+                token_type = Type::Identifier;
+            }
 
+            token_content += &c.to_string();
+        } else if c == ' ' {
+            if !token_content.is_empty() {
+                tokens.push(Token {t: token_type, c: token_content.clone()});
+                token_type = Type::None;
+                token_content.clear();
+            }
+        }
+    }
+
+    if !token_content.is_empty() {
+        tokens.push(Token {t: token_type, c: token_content.clone()});
+    }
 
     tokens
 }
--- tests/tokenizer_test.rs
+++ tests/tokenizer_test.rs
@@ -26,3 +26,25 @@ fn test_empty_string() {
 
     assert_eq!(expected, actual);
 }
+
+#[test]
+fn test_single_identifier() {
+    let expected: Vec<Token> = vec![
+        Token {t: Type::Identifier, c: String::from("hola")}
+    ];
+    let actual: Vec<Token> = tokenize("hola", get_es_keywords());
+
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_multiple_identifiers() {
+    let expected: Vec<Token> = vec![
+        Token {t: Type::Identifier, c: String::from("cosas")},
+        Token {t: Type::Identifier, c: String::from("mundo")},
+        Token {t: Type::Identifier, c: String::from("ayuda")},
+    ];
+    let actual: Vec<Token> = tokenize("cosas mundo ayuda", get_es_keywords());
+
+    assert_eq!(expected, actual);
+}
